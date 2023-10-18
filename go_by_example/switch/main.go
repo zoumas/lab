ca58b3ffdd4bc3e134c4d699a9b211b5b8f3fb24
zoumas <zoumas@protonmail.com>
2023-10-18 12:24:31 +0300

    Go by Example: Switch

---
+++ go_by_example/switch/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	i := 2
+	fmt.Print("Write ", i, " as ")
+	switch i {
+	case 1:
+		fmt.Println("one")
+	case 2:
+		fmt.Println("two")
+	case 3:
+		fmt.Println("three")
+	}
+
+	switch time.Now().Weekday() {
+	case time.Saturday, time.Sunday:
+		fmt.Println("It's the weekend")
+	default:
+		fmt.Println("It's a weekday")
+	}
+
+	switch t := time.Now(); {
+	case t.Hour() < 12:
+		fmt.Println("It's before noon")
+	default:
+		fmt.Println("It's after noon")
+	}
+
+	identify := func(i any) {
+		switch t := i.(type) {
+		case bool:
+			fmt.Println("I'm a bool")
+		case int:
+			fmt.Println("I'm an int")
+		default:
+			fmt.Printf("Don't know type %T\n", t)
+		}
+	}
+
+	identify(true)
+	identify(1)
+	identify("hey")
+}
