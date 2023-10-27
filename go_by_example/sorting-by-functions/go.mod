f7e7e84045815b899229bc082937ff803390883f
zoumas <zoumas@protonmail.com>
2023-10-27 15:50:47 +0300

    Go by Example: Sorting by Functions

---
+++ go_by_example/sorting-by-functions/go.mod
@@ -0,0 +1,3 @@
+module github.com/zoumas/lab/go_by_example/sorting-by-functions
+
+go 1.21.3
---
+++ go_by_example/sorting-by-functions/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"cmp"
+	"fmt"
+	"slices"
+)
+
+func main() {
+	fruits := []string{"peach", "banana", "kiwi"}
+
+	lenCmp := func(a, b string) int {
+		return cmp.Compare(len(a), len(b))
+	}
+
+	slices.SortFunc(fruits, lenCmp)
+	fmt.Println(fruits)
+
+	type Person struct {
+		name string
+		age  int
+	}
+
+	people := []Person{
+		{name: "Jax", age: 37},
+		{name: "TJ", age: 25},
+		{name: "Alex", age: 72},
+	}
+
+	// sort a slice of structs by a field
+	slices.SortFunc(people, func(a, b Person) int {
+		return cmp.Compare(a.age, b.age)
+	})
+	fmt.Println(people)
+}
